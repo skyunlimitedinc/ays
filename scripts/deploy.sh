5165594a7cabe15ce77242096e2f28ba95b6e1f0
Chris McGee <chris@chrismcgee.info>
2020-10-13 10:55:10 -0400

    Ansible Provisioning
    * Creates `.env` via Ansible

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,15 @@ set -e
 ssh-keyscan -H $IP >>~/.ssh/known_hosts
 
 # Copy files to the server
-rsync -aO --chown=www-data:www-data --fake-super --exclude '.git' ./ $USER_NAME@$IP:$DEPLOY_PATH
+rsync -aO --chown=www-data:www-data --fake-super --exclude='.git' --exclude='.env' --del ./ $USER_NAME@$IP:$DEPLOY_PATH
+
+# Install dependencies
+ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && composer install --no-progress --no-interaction --optimize-autoloader --no-dev'
+ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && npm install'
+
+# Prep for production
+ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && npm run production'
+ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && php artisan key:generate'
+ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && php artisan storage:link'
+ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && php artisan config:cache'
+ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && php artisan route:cache'
