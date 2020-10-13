e0bcd55b9c062fc9d3e1b4e1fb2440c45a47571b
Chris McGee <chris@chrismcgee.info>
2020-10-13 11:09:58 -0400

    Double Quotes
    * Changes single quotes to double quotes in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,12 +6,12 @@ ssh-keyscan -H $IP >>~/.ssh/known_hosts
 rsync -aO --chown=www-data:www-data --fake-super --exclude='.git' --exclude='.env' --del ./ $USER_NAME@$IP:$DEPLOY_PATH
 
 # Install dependencies
-ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && composer install --no-progress --no-interaction --optimize-autoloader --no-dev'
-ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && npm install'
+ssh -t $USER_NAME@$IP "cd $DEPLOY_PATH && composer install --no-progress --no-interaction --optimize-autoloader --no-dev"
+ssh -t $USER_NAME@$IP "cd $DEPLOY_PATH && npm install"
 
 # Prep for production
-ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && npm run production'
-ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && php artisan key:generate'
-ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && php artisan storage:link'
-ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && php artisan config:cache'
-ssh -t $USER_NAME@$IP 'cd $DEPLOY_PATH && php artisan route:cache'
+ssh -t $USER_NAME@$IP "cd $DEPLOY_PATH && npm run production"
+ssh -t $USER_NAME@$IP "cd $DEPLOY_PATH && php artisan key:generate"
+ssh -t $USER_NAME@$IP "cd $DEPLOY_PATH && php artisan storage:link"
+ssh -t $USER_NAME@$IP "cd $DEPLOY_PATH && php artisan config:cache"
+ssh -t $USER_NAME@$IP "cd $DEPLOY_PATH && php artisan route:cache"
