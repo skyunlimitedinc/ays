cf33b8b3a87d6a2a6c2da5f27316d5d01e4c9a96
Chris McGee <chris@chrismcgee.info>
2020-10-13 08:50:27 -0400

    rsync -a

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ set -e
 ssh-keyscan -H $IP >>~/.ssh/known_hosts
 
 # Copy files to the server
-rsync -rlptOog --chown=www-data:www-data --fake-super --exclude '.git' ./ $USER_NAME@$IP:$DEPLOY_PATH
+rsync -a --chown=www-data:www-data --fake-super --exclude '.git' ./ $USER_NAME@$IP:$DEPLOY_PATH
