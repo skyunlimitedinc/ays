ae13c9043fd8dca9c6e40624cd2025698f87b901
Chris McGee <chris@chrismcgee.info>
2021-05-20 10:38:51 -0400

    🔥 Removes old Travis scripts

--- .github/scripts/decrypt_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$ENV_PASSPHRASE" \
---output "$HOME/.env" "$HOME/.github/secrets/.env.gpg"
